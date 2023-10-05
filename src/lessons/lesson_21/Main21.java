e403f18768b4b741df464d56ce0b6d8d72683d3e
aidin <aidinmaxim85@gmail.com>
2023-10-05 15:28:31 +0200

    lesson_21

---
+++ src/lessons/lesson_21/Main21.java
@@ -0,0 +1,20 @@
+package lessons.lesson_21;
+
+/*
+@date 04.10.2023
+@author Sergey Bugaienko
+*/
+
+public class Main21 {
+
+    public static void main(String[] args) {
+
+        // Мы можем вызвать статический метод без создания объекта
+        int sum = MathUtil.symOfInts(1, 2, 3);
+        System.out.println("Сумма: " + sum);
+
+        System.out.println(Math.sqrt(16));
+
+
+    }
+}
---
+++ src/lessons/lesson_21/MathUtil.java
@@ -0,0 +1,18 @@
+package lessons.lesson_21;
+
+/*
+@date 04.10.2023
+@author Sergey Bugaienko
+*/
+
+public class MathUtil {
+
+    public static int symOfInts(int x, int... ints) {
+        int sum = x;
+        for (int value: ints) {
+            sum += value;
+        }
+
+        return sum;
+    }
+}
